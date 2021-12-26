a29decd65ea8cc8970a1f196c681b337ba3aa5b0
Leo Kling <leo.kling.info@gmail.com>
2021-12-26 17:02:21 +0100

    added trpc and pokenode-ts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import type { NextPage } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
+import { PokemonClient } from "pokenode-ts";
+
+const MAX_DEX_NUMBER = 493;
+
+const getRandomNumber = (notThis?: number) => {
+  let r = Math.floor(Math.random() * MAX_DEX_NUMBER + 1);
+  while (r == notThis) {
+    r = Math.floor(Math.random() * MAX_DEX_NUMBER + 1);
+  }
+  return r
+};
+
+const Home: NextPage = ()  => {
+  const pokemonID1 = getRandomNumber();
+  const pokemonID2 = getRandomNumber(pokemonID1);
 
 
-const Home: NextPage = () => {
+  const pokeApi = new PokemonClient();
+  const pokemon1 =  pokeApi.getPokemonById(pokemonID1);
+  const pokemon2 = pokeApi.getPokemonById(pokemonID2);
+
   return (
     <div>
       <Head>
@@ -15,15 +34,19 @@ const Home: NextPage = () => {
 
         <div className="border flex justify-center items-center">
           <div className="flex-col pb-2">
-            <div className="pt-2">Pokemon 1</div>
-            <div className="p-2"/>
-            <button className="bg-white text-black font-bold py-2 px-4 rounded-full">Rounder</button>
+            <div className="pt-2">{pokemonID1}</div>
+            <div className="p-2" />
+            <button className="bg-white text-black font-bold py-2 px-4 rounded-full">
+              Rounder
+            </button>
           </div>
           <div className="px-8">or</div>
           <div className="flex-col pb-2">
-            <div className="pt-2">Pokemon 2</div>
-            <div className="p-2"/>
-            <button className="bg-white text-black font-bold py-2 px-4 rounded-full">Rounder</button>
+            <div className="pt-2">{pokemonID2}</div>
+            <div className="p-2" />
+            <button className="bg-white text-black font-bold py-2 px-4 rounded-full">
+              Rounder
+            </button>
           </div>
         </div>
       </main>
@@ -31,6 +54,14 @@ const Home: NextPage = () => {
       <footer></footer>
     </div>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  return {
+    props: {
+      data: null,
+    },
+  };
 };
 
 export default Home;
