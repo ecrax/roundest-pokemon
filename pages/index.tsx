9e2783a944bf9fbb2860048df2ae1d9a7dc1a6b7
Leo Kling <leo.kling.info@gmail.com>
2021-12-27 19:53:12 +0100

    still stupid bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,7 +58,10 @@ const Home: NextPage = () => {
         <h1 className="text-4xl text-center">Which Pokemon is Rounder?</h1>
 
         <div className="flex justify-center items-center">
-          
+          <PokemonCard pokemon={pokemonPair.firstPokemon} vote={vote} />
+
+          <div className="px-8 italic">or</div>
+          <PokemonCard pokemon={pokemonPair.secondPokemon} vote={vote} />
         </div>
       </main>
 
@@ -71,17 +74,7 @@ const PokemonCard: React.FC<{ vote: Function; pokemon: any }> = (props) => {
   return (
     <div className="flex-col pb-2 justify-items-center flex items-center">
       <div className="pt-2 capitalize text-center">{props.pokemon.name}</div>
-      <div className="p-2" />
-      <Image src={props.pokemon.spriteUrl} width={256} height={256} />
-      <div className="p-2" />
-      <button
-        onClick={() => {
-          props.vote(props.pokemon.id);
-        }}
-        className="bg-white text-black font-bold py-2 px-4 rounded-full"
-      >
-        Rounder
-      </button>
+      
     </div>
   );
 };
