0ba328d7e94f396738292d66be9684a32e186507
Leo Kling <leo.kling.info@gmail.com>
2021-12-28 15:26:51 +0100

    loading

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,15 @@ import Image from "next/image";
 import { GetServerSideProps } from "next";
 import { getPokemonFromDb } from "@utils/getPokemonFromDb";
 import { useState } from "react";
+import { isFloat32Array } from "util/types";
 
 const Home: NextPage<{ pokemonPair: any }> = (props) => {
   const [pokemonPair, setPokemonPair] = useState(props.pokemonPair);
+  const [isLoading, setIsLoading] = useState(false);
 
   const vote = async (id: number) => {
     if (!id) return;
-    
+
     if (id == pokemonPair.firstPokemon.id) {
       const { error } = await supabase.rpc("incrementvotesfor", {
         x: 1,
@@ -37,8 +39,9 @@ const Home: NextPage<{ pokemonPair: any }> = (props) => {
       });
       if (error2) console.error(error2);
     }
-
+    setIsLoading(true);
     setPokemonPair(await getPokemonFromDb());
+    setIsLoading(false);
   };
 
   return (
@@ -50,12 +53,12 @@ const Home: NextPage<{ pokemonPair: any }> = (props) => {
 
       <main className="flex flex-col justify-between">
         <h1 className="text-4xl text-center">Which Pokemon is Rounder?</h1>
-        <div className="p-8"/>
+        <div className="p-8" />
         <div className="flex justify-center items-center">
-          <PokemonCard pokemon={pokemonPair.firstPokemon} vote={vote} />
+          <PokemonCard pokemon={pokemonPair.firstPokemon} vote={vote} disabled={isLoading} />
 
           <div className="px-16 italic">or</div>
-          <PokemonCard pokemon={pokemonPair.secondPokemon} vote={vote} />
+          <PokemonCard pokemon={pokemonPair.secondPokemon} vote={vote} disabled={isLoading} />
         </div>
       </main>
 
@@ -67,9 +70,14 @@ const Home: NextPage<{ pokemonPair: any }> = (props) => {
 const PokemonCard: React.FC<{
   vote: Function;
   pokemon: { name: string; spriteUrl: string; id: number } | undefined;
+  disabled: boolean;
 }> = (props) => {
   return (
-    <div className="flex-col pb-2 justify-items-center flex items-center">
+    <div
+      className={`flex-col pb-2 justify-items-center flex items-center transition-opacity ${
+        props.disabled && "opacity-0"
+      }`}
+    >
       <div className="pt-2 capitalize text-center">{props.pokemon?.name}</div>
       <div className="p-2" />
       <Image
@@ -77,6 +85,7 @@ const PokemonCard: React.FC<{
         width={256}
         height={256}
         alt="Pokemon Image"
+        className="animate-fade-in"
       />
       <div className="p-2" />
       <button
@@ -84,6 +93,7 @@ const PokemonCard: React.FC<{
           props.vote(props.pokemon?.id);
         }}
         className="bg-white text-black font-bold py-2 px-4 rounded-full"
+        disabled={props.disabled}
       >
         This one
       </button>
